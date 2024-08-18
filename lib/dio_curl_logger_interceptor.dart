6725b15a708ea453a24f77d5e17f207eb9f23021
Abdallah Odeh <abdallah@odeh-bros.com>
2024-08-18 12:11:08 +0300

    improved body parsing

--- lib/dio_curl_logger_interceptor.dart
+++ lib/dio_curl_logger_interceptor.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class DioCurlLoggerInterceptor extends Interceptor {
   const DioCurlLoggerInterceptor();
@@ -35,18 +36,57 @@ class DioCurlLoggerInterceptor extends Interceptor {
       }
     });
 
-    if (options.data != null) {
-      // FormData can't be JSON-serialized, so keep only their fields attributes
-      if (options.data is FormData) {
-        for (final field in options.data.fields) {
-          components.add('--form \'${field.key}="${field.value}"\'');
-        }
-      } else {
-        final data = json.encode(options.data).replaceAll('"', '\\"');
-        components.add('-d "$data"');
-      }
-    }
+    components.addAll(_getData(options));
 
     return components.join(' \\\n\t');
+  }
+
+  List<String> _getData(RequestOptions options) {
+    if (options.data == null) return const <String>[];
+
+    final contentType = _getContentType(options);
+
+    switch(contentType.toLowerCase()) {
+      case 'application/json': //raw json
+        return ['--data \'${options.data}\''];
+      case 'application/x-www-form-urlencoded': // x-www-form-urlencoded
+        return _getBody('data-urlencode', options.data);
+      case 'multipart/form-data': //form-data
+        return _getBody('form', options.data);
+      default:
+        debugPrint('Could not parse body of type $contentType');
+    }
+    return const <String>[];
+  }
+
+  List<String> _getBody(final String prefix, final dynamic data) {
+    if (data == null) return const <String>[];
+    final components = <String>[];
+    if (data is FormData) {
+      for (final field in data.fields) {
+        components.add('--$prefix \'${field.key}="${field.value}"\'');
+      }
+      if (kDebugMode && data.files.isNotEmpty) {
+        debugPrint('Form data files cannot be added to cURL');
+      }
+    } else if (data is Map) {
+      for (final field in data.entries) {
+        components.add('--$prefix \'${field.key}="${field.value}"\'');
+      }
+    } else if (kDebugMode) {
+      debugPrint('Could not parse body of type ${data.runtimeType}');
+    }
+    return components;
+  }
+
+  String _getContentType(RequestOptions options) {
+    final headers = options.headers;
+    const key = 'content-type';
+    for (final header in headers.entries) {
+      if (header.key.toLowerCase() == key) {
+        return header.value;
+      }
+    }
+    return 'application/json';
   }
 }
